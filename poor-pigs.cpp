ad138d9e24705ce4e9e6c90c5949976633831ff8
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2018-10-31 21:38:49 +0530

    Create poor-pigs.cpp

---
+++ poor-pigs.cpp
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/poor-pigs/
+
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int pigs=0;
+        while(pow((minutesToTest/minutesToDie+1),pigs)<buckets)
+            pigs++;
+        return pigs;
+    }
+};
