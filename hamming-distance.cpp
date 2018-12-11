87fe220f58d0315bedd24634463ad93bc3a154d8
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2018-12-11 23:41:45 +0530

    Create hamming-distance.cpp

---
+++ hamming-distance.cpp
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/hamming-distance/
+
+class Solution:
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        return str(bin(x^y))[2:].count("1")
+        
