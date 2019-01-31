3cac7e05cf069d9feedbba0f8300f039f87875b1
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-01-31 23:28:30 +0530

    Create sort-and-reverse-vector.cpp

---
+++ sort-and-reverse-vector.cpp
@@ -0,0 +1,27 @@
+//https://practice.geeksforgeeks.org/problems/sort-and-reverse-vector/1
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+//User function Template for C++
+v sort(v.begin(),v.end());
+    return v;
+}
+vector<int> reverseVector(vector<int>v)
+{
+     //Your code here. Use library function
+     reverse(v.begin(),v.end());
+    return v;
+}ector<int> sortVector(vector<int>v)
+{
+   //Your code here. Use library function
+   sort(v.begin(),v.end());
+    return v;
+}
+vector<int> reverseVector(vector<int>v)
+{
+     //Your code here. Use library function
+     reverse(v.begin(),v.end());
+    return v;
+}
