9ff78061cac67d1be97a059f2dec322f3895068b
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-07-31 10:03:14 +0530

    Update inversion-of-array.cpp

--- inversion-of-array.cpp
+++ inversion-of-array.cpp
@@ -1,6 +1,6 @@
 //https://practice.geeksforgeeks.org/problems/inversion-of-array/0
 
-
+//Brute Force Solution
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -24,3 +24,58 @@ int main() {
 	}
 	return 0;
 }
+
+
+
+
+
+//Efficient Merge Sort Solution
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+long long int merge(int arr[], int l, int m, int r, int& size){
+    long long int result=0;
+    int dummy[size];
+    int i=l, j=m+1, k=l;
+    while(i<=m && j<=r){
+        if(arr[j]<arr[i]){ dummy[k++]=arr[j++]; result+=(m+1-i); }
+        else dummy[k++]=arr[i++];
+    }
+    while(i<=m) dummy[k++]=arr[i++];
+    while(j<=r) dummy[k++]=arr[j++];
+    for(int i=l;i<=r;i++)
+        arr[i]=dummy[i];
+    return result;
+}
+
+long long int mergeSort(int arr[], int l, int r, int& size){
+    if(l==r) return 0;
+    int m=(l+r)/2;
+    long long int result=0;
+    result=mergeSort(arr,l,m,size)+mergeSort(arr,m+1,r,size);
+    result+=merge(arr,l,m,r,size);
+    return result;
+}
+
+
+
+int main() {
+	//code
+	int testcase;
+	cin>>testcase;
+	while(testcase--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	        cin>>arr[i];
+	    long long int result=0;
+	    result=mergeSort(arr,0,n-1,n);
+	    //for(int i=0;i<n;i++) cout<<arr[i]<<" :";
+	    cout<<result<<endl;
+	}
+	return 0;
+}
+
