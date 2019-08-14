f611c2cb7fdfea05a72824b8dec45c8e91f5591c
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-08-14 18:29:47 +0530

    Create delete-without-head-pointer.cpp

---
+++ delete-without-head-pointer.cpp
@@ -0,0 +1,11 @@
+//https://practice.geeksforgeeks.org/problems/delete-without-head-pointer/1
+
+// This function should delete node from linked list. The function
+// may assume that node exists in linked list and is not last node
+// node: reference to the node which is to be deleted
+void deleteNode(Node *node)
+{
+   // Your code here
+   node->data=node->next->data;
+   node->next=node->next->next;
+}
